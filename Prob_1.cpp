eb379b0212b33b171fc977d5ccf22d04a4f63c68
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-08-07 23:50:48 +0530

    Add files via upload

---
+++ Prob_1.cpp
@@ -0,0 +1,21 @@
+//AUTHOR:K.RAKSHANA KUMARI DATE:8-7-23
+//debug the code
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+ int t;
+ cin>>t;
+ while(t>0)
+  { 
+   int X,Y,prize_top10,prize_11to100;
+   cin>>X>>Y;
+   prize_top10 = 10*X;           
+    //There was a logical error in this line. Rank 11 to 100 includes 90 participants. Not 89.
+    prize_11to100 = 90*Y;        
+    cout<<prize_top10+prize_11to100<<endl;
+    t--; 
+   }
+ return 0;
+}
